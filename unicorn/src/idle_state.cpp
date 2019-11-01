493ea2463c35437371db3228e11a6bccccb5d2c9
Daniel Stenkap <daniel.stenekap@gmail.com>
2019-11-01 15:47:41 +0100

    started to define idle_state.cpp

---
+++ unicorn/src/idle_state.cpp
@@ -0,0 +1,20 @@
+#include<idle_state.h>
+
+IDLEState::IDLEState(std::string &cmd_msg, cmd_struct_ &cmd_struct)
+{
+
+}
+
+void IDLEState::Run()
+{
+
+//Parse msg
+//Logic for msg handling
+//Write reason for exit to msg pointer lvl up
+
+}
+
+
+
+
+//terminate
