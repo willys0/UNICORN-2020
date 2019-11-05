7770c1bbc0f947f6b1f58d43935205734ba5146b
Janne <jsn15011@student.mdh.se>
2019-11-05 09:22:18 +0100

    added refuse bin to seperate header and implementation files

---
+++ unicorn/include/unicorn/refuse_bin.h
@@ -0,0 +1,9 @@
+/** @brief Storage class for refuse bin position*/
+class RefuseBin
+{
+public:
+	RefuseBin();
+	float x;
+	float y;
+	float yaw;
+};
---
+++ unicorn/src/refuse_bin.cpp
@@ -0,0 +1,8 @@
+#include "refuse_bin.h"
+
+RefuseBin::RefuseBin()
+{
+	x = 8.0;
+	y = -2.0;
+	yaw = 0;
+}
