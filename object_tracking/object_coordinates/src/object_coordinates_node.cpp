f34698e4969c2b8e9586ff9313b70b90f23d5c9c
peterandersson91 <peteranderssonkolback@gmail.com>
2019-12-04 12:52:00 +0100

    Added closest human

--- object_tracking/object_coordinates/src/object_coordinates_node.cpp
+++ object_tracking/object_coordinates/src/object_coordinates_node.cpp
@@ -4,18 +4,13 @@
 #include <darknet_ros_msgs/BoundingBoxes.h>
 #include <Eigen/Dense>
 #include <Eigen/LU>
-
 #include <geometry_msgs/TransformStamped.h>
 #include <tf/transform_listener.h>
-
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
 #include <string>
-
 using namespace Eigen;
-
 geometry_msgs::PointStamped msg;
-
 void callback(const sensor_msgs::Image::ConstPtr& im_msg, const darknet_ros_msgs::BoundingBoxes::ConstPtr& bb_msg)
 {
 	int xmin, xmax, ymin, ymax, width;
@@ -27,31 +22,36 @@ void callback(const sensor_msgs::Image::ConstPtr& im_msg, const darknet_ros_msgs
 	xmin = xmax = ymin = ymax = -1;
 	width = 1280;
 	depths = NULL;
-
-	//filter out anything but humans
-	if (bb_msg->bounding_boxes[0].id == 0) {
-		//get the middle pixel in the bounding box
-		//save the bounding box as integers
-		int x = (bb_msg->bounding_boxes[0].xmin+bb_msg->bounding_boxes[0].xmax)/2;
-		int y = (bb_msg->bounding_boxes[0].ymin+bb_msg->bounding_boxes[0].ymax)/2;
+	int size = 0;
 	
-		// Get a pointer to the depth values casting the data
-		depths = (float*)(&im_msg->data[0]);
-	
-		coordinates(0) = x;
-		coordinates(1) = y;
-		coordinates(2) = depths[x+y*im_msg->width];
-	
-		//to do: investigate
-		//Intrinsic matrix to compensate for camera focal lengt, zoom and distortion
-		intrinsic << 	678.2827758789062,	0,					647.6021728515625,
-						0,					678.2827758789062,	370.912109375, 	
-						0,					0,					1.0;
-
-
+	coordinates(2) = 10;
+	//look for closest human with for loop
+	for(int i = 0; i < sizeof(bb_msg->bounding_boxes)/sizeof(bb_msg->bounding_boxes[0]); i++) {
+		//filter out anything but humans
+		if (bb_msg->bounding_boxes[i].id == 0) {
+			//get the middle pixel in the bounding box
+			//save the bounding box as integers
+			int x = (bb_msg->bounding_boxes[i].xmin+bb_msg->bounding_boxes[i].xmax)/2;
+			int y = (bb_msg->bounding_boxes[i].ymin+bb_msg->bounding_boxes[i].ymax)/2;
+			// Get a pointer to the depth values casting the data
+			depths = (float*)(&im_msg->data[i]);
+			//Intrinsic matrix to compensate for camera focal lengt, zoom and distortion
+			intrinsic << 	678.2827758789062,	0,					647.6021728515625,
+							0,					678.2827758789062,	370.912109375, 	
+							0,					0,					1.0;
+			
+			//save the closest human
+			if(coordinates(2) > depths[x+y*im_msg->width){
+				coordinates(0) = x;
+				coordinates(1) = y;
+				coordinates(2) = depths[x+y*im_msg->width];
+			}
+		}
+	}
+	//if no human was found
+	if(coordinates(2) != 10) {			 
 		//convert from pixel coordinates to world coordinates
 		coordinates = intrinsic.inverse()*coordinates;
-	
 		//transcribe values to message
 		msg.point.x = coordinates(0);
 		msg.point.y = coordinates(1);
@@ -60,13 +60,11 @@ void callback(const sensor_msgs::Image::ConstPtr& im_msg, const darknet_ros_msgs
 		msg.header.frame_id = im_msg->header.frame_id;
 	}
 }
-
 /**
  * Node main function
  */
 int main(int argc, char** argv) 
 {
-
 	//initialize
     ros::init(argc, argv, "tx2_object_coordinates");
     ros::NodeHandle n;
