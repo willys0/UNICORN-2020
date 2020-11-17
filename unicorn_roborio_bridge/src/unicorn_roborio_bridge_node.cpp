8e7de883ab14b928cdff97f34d55fb1377bc10f7
Christian Mortensen <cnmrtnsn@gmail.com>
2020-11-17 12:57:28 +0100

    Add action for controlling the lift

--- unicorn_roborio_bridge/src/unicorn_roborio_bridge_node.cpp
+++ unicorn_roborio_bridge/src/unicorn_roborio_bridge_node.cpp
@@ -2,10 +2,22 @@
 
 #include <algorithm>
 
+#include <actionlib/server/simple_action_server.h>
+
 #include <std_msgs/Int32.h>
 #include <sensor_msgs/LaserScan.h>
 
 #include <unicorn_roborio_bridge/RioMasterMsg.h>
+#include <unicorn_roborio_bridge/RunLiftAction.h>
+
+#define RIO_LIFT_ACTION_STOP    0
+#define RIO_LIFT_ACTION_PICKUP  1
+#define RIO_LIFT_ACTION_DROPOFF 2
+
+#define RIO_LIFT_STATE_IDLE  0
+#define RIO_LIFT_STATE_ERROR 6
+
+typedef actionlib::SimpleActionServer<unicorn_roborio_bridge::RunLiftAction> LiftActionServer;
 
 ros::Subscriber rio_sub;
 
@@ -21,6 +33,86 @@ float                  lidar_freq;
 // geometry_msgs::Point uwb_pos;
 
 int n_measures;
+
+void execute_lift(const unicorn_roborio_bridge::RunLiftGoalConstPtr& goal, LiftActionServer* as, ros::NodeHandle nh) {
+    ros::Publisher run_lift_pub;
+    std_msgs::Int32 msg;
+    int dir;
+
+    if(goal->direction == goal->DIRECTION_PICKUP) {
+        dir = RIO_LIFT_ACTION_PICKUP;
+        ROS_INFO("[Roborio Bridge] Running lift pickup routine.");
+    }
+    else if(goal->direction == goal->DIRECTION_DROPOFF) {
+        dir = RIO_LIFT_ACTION_DROPOFF;
+        ROS_INFO("[Roborio Bridge] Running lift dropoff routine.");
+    }
+    else {
+        // TODO: Handle wrong input value
+        ROS_ERROR("[Roborio Bridge] Invalid lift direction given to lifting action.");
+        as->setAborted();
+        return;
+    }
+
+    // Wait for lift to be idle
+    ROS_INFO("[Roborio Bridge] Waiting for lift to be idle...");
+    while(lift_state.data != RIO_LIFT_STATE_IDLE) {
+        if(!ros::ok()) {
+            as->setAborted();
+            return;
+        }
+    }
+
+    ROS_INFO("[Roborio Bridge] Sending command to lift.");
+    run_lift_pub = nh.advertise<std_msgs::Int32>("/TX2_unicorn_picking_routine", 1);
+    msg.data = dir;
+    run_lift_pub.publish(msg);
+
+    // Wait for lift to move out of idle state
+    while(ros::ok() && lift_state.data == RIO_LIFT_STATE_IDLE) {
+        if(as->isPreemptRequested()) {
+            break;
+        }
+
+        // TODO: May need to retry sending message here
+
+        ros::spinOnce();
+    }
+
+    ROS_INFO("[Roborio Bridge] Waiting for lift to finish...");
+
+    // Wait for lift to be done
+    while(ros::ok()) {
+        if(as->isPreemptRequested()) {
+
+            msg.data = RIO_LIFT_ACTION_STOP;
+            run_lift_pub.publish(msg);
+
+            ROS_INFO("[Roborio Bridge] Lift action preempted.");
+            as->setPreempted();
+
+            // TODO: Shall we reverse the lift back to initial position?
+            break;
+        }
+
+        if(lift_state.data == RIO_LIFT_STATE_IDLE) {
+            ROS_INFO("[Roborio Bridge] Lift action succeeded.");
+            as->setSucceeded();
+            break;
+        }
+        else if(lift_state.data == RIO_LIFT_STATE_ERROR) {
+            // TODO: Handle error better
+            ROS_ERROR("[Roborio Bridge] Lift action failed.");
+            as->setAborted();
+            break;
+        }
+
+        ros::spinOnce();
+    }
+
+    run_lift_pub.shutdown();
+
+}
 
 void rio_cb(const unicorn_roborio_bridge::RioMasterMsgConstPtr& msg) {
 
@@ -110,8 +202,12 @@ int main(int argc, char** argv) {
 
     rio_sub = nh.subscribe("/RIO_publisher", 10, &rio_cb);
 
+    LiftActionServer lift_server(nh, "/lift/lift_action", boost::bind(&execute_lift, _1, &lift_server, nh), false);
+    lift_server.start();
+
     ros::Timer lidar_timer = nh.createTimer(ros::Duration(1.0 / lidar_freq), &lidar_pub_timeout);
     ros::Timer lift_timer = nh.createTimer(ros::Duration(1.0 / lift_freq), &lift_pub_timeout);
+
 
     ros::spin();
 }
