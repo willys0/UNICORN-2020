a959a08d97ec4bc19c1a7dd9445a76c335e1426a
Christian Mortensen <cnmrtnsn@gmail.com>
2020-11-17 11:11:27 +0100

    Docking state now calls the docking controller action

--- unicorn_state_machine/src/unicorn_state_machine/dock_state.cpp
+++ unicorn_state_machine/src/unicorn_state_machine/dock_state.cpp
@@ -2,6 +2,13 @@
 
 #include <unicorn_state_machine/idle_state.h>
 #include <unicorn_state_machine/lift_state.h>
+#include <unicorn_state_machine/undock_state.h>
+
+#include <actionlib/client/simple_action_client.h>
+
+#include <unicorn_docking/DockAction.h>
+
+typedef actionlib::SimpleActionClient<unicorn_docking::DockAction> DockActionClient;
 
 DockState::DockState(int lift_cmd, ros::NodeHandle node) :
     State::State(node)
@@ -12,15 +19,29 @@ DockState::DockState(int lift_cmd, ros::NodeHandle node) :
 State* DockState::run() {
     ROS_INFO("[DockState] lift: %d", lift_cmd_);
 
+    ROS_INFO("[DockState] Waiting for dock action server...");
+    DockActionClient client("/dock", true);
+    client.waitForServer();
+
+
     // TODO: Try to find a tag, if fail go to idle state (?)
     // return new IdleState(nh_);
 
-    // TODO: Call the docking controller to initiate a dock
+    // Call the docking controller to initiate a dock
 
-    // TODO: If failed to dock (define failed), do an undock
-    // return new UndockState(lift_cmd_, nh_);
+    unicorn_docking::DockGoal goal;
+    client.sendGoal(goal);
+    client.waitForResult();
 
-    // TODO: Go to lift state if docked
-    return new LiftState(lift_cmd_, nh_);
+    if(client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+        // Go to lift state if docked
+        return new LiftState(lift_cmd_, nh_);
+    }
+    else {
+        return new UndockState(lift_cmd_, nh_);
+    }
+
+    // TODO: Handle errors
+    return new IdleState(nh_);
 
 }
