cc8d64133fa3508c2dde9be190da87f6e7b29df0
krilleshomies <kristoffer.lindve@gmail.com>
2020-11-22 19:20:19 +0100

    started buidling framework for tracking

---
+++ tracking_lidar/src/tracking_lidar.cpp
@@ -0,0 +1,89 @@
+#include "tracking_lidar.h"
+
+
+static geometry_msgs::TransformStamped transform_frames;
+int main(int argc, char** argv){
+  ros::init(argc, argv, "tracking_lidar");
+
+  ros::NodeHandle nh;
+  ROS_INFO("Started Lidar Tracking node");
+  tracting_lidar tracting_lidar_interface;
+  ros::Rate r(100.0);
+
+
+  //tf2_ros::Buffer tf_buffer;
+  //tf2_ros::TransformListener tf2_listener(tf_buffer);
+  //transform_frames = tf_buffer.lookupTransform("chassis_link", "map", ros::Time(0), ros::Duration(1.0) );
+
+  while(ros::ok())
+  {
+    //ROS_INFO("Tracking node running");
+    ros::spinOnce();
+    r.sleep();
+  }
+}
+
+
+tracting_lidar::tracting_lidar()
+{
+  odometry_sub_ = n_.subscribe("/odometry/filtered", 10, &tracting_lidar::odomCallback, this);
+  map_sub_ = n_.subscribe("/map", 10, &tracting_lidar::mapCallback, this);
+  scan_sub_ = n_.subscribe("/frontLidar/scan", 10, &tracting_lidar::scanCallback, this);
+  //odometry_transform_pub_ = n_.advertise<nav_msgs::Odometry>("/wheel_encoder/odom_transformed", 10,true);
+}
+
+void tracting_lidar::odomCallback(const nav_msgs::Odometry& odometry)
+{
+    ROS_INFO("New odom Message!");
+}
+
+void tracting_lidar::mapCallback(const nav_msgs::OccupancyGrid& map)
+{
+    ROS_INFO("New map Message!");
+}
+
+void tracting_lidar::scanCallback(const sensor_msgs::LaserScan& scan)
+{
+    ROS_INFO("New scan Message!");
+}
+
+
+/*
+static geometry_msgs::TransformStamped transform_frames;
+int main(int argc, char** argv){
+  ros::init(argc, argv, "tracking_lidar");
+  ROS_INFO("Started Lidar Tracking node");
+  odom_transform odom_transform_interface;
+  ros::Rate r(100.0);
+
+
+  tf2_ros::Buffer tf_buffer;
+  tf2_ros::TransformListener tf2_listener(tf_buffer);
+  transform_frames = tf_buffer.lookupTransform("chassis_link", "lift_link", ros::Time(0), ros::Duration(1.0) );
+
+  while(ros::ok())
+  {
+    odom_transform_interface.publishmsg();
+    ros::spinOnce();
+    r.sleep();
+  }
+}
+
+/*
+odom_transform::odom_transform()
+{
+  odometry_sub_ = n_.subscribe("/wheel_encoder/odom", 10, &odom_transform::msgCallback, this);
+  odometry_transform_pub_ = n_.advertise<nav_msgs::Odometry>("/wheel_encoder/odom_transformed", 10,true);
+}
+
+void odom_transform::msgCallback(const nav_msgs::Odometry& odometry)
+{
+  odometry_data_ = odometry;
+  tf2::doTransform((odometry.pose.pose), (odometry_data_.pose.pose), transform_frames);
+}
+
+void odom_transform::publishmsg()
+{
+  odometry_transform_pub_.publish(odometry_data_);
+}
+*/
---
+++ tracking_lidar/src/tracking_lidar.h
@@ -0,0 +1,70 @@
+#ifndef TRACKING_LIDAR_H
+#define TRACKING_LIDAR_H
+
+/* ROS */
+#include <ros/ros.h>
+#include <tf/tf.h>
+#include <nav_msgs/Odometry.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <sensor_msgs/LaserScan.h>
+#include <tf2_ros/transform_listener.h>
+
+
+
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
+/* C / C++ */
+#include <iostream>
+#include <termios.h>
+
+class tracting_lidar
+{
+public:
+	tracting_lidar();
+	void odomCallback(const nav_msgs::Odometry& odometry);
+	void mapCallback(const nav_msgs::OccupancyGrid& map);
+	void scanCallback(const sensor_msgs::LaserScan& scan);
+	void publishmsg();
+private:
+	ros::NodeHandle n_;
+	nav_msgs::Odometry odometry_data_;
+	ros::Subscriber odometry_sub_;
+	ros::Subscriber map_sub_;
+	ros::Subscriber scan_sub_;
+
+};
+#endif
+
+
+/*
+#ifndef ODOMETRY_TRANSFORM_H
+#define ODOMETRY_TRANSFORM_H
+
+
+#include <ros/ros.h>
+#include <tf/tf.h>
+#include <nav_msgs/Odometry.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf/transform_broadcaster.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/buffer_interface.h>
+
+
+#include <iostream>
+#include <termios.h>
+
+class odom_transform
+{
+public:
+	odom_transform();
+	void msgCallback(const nav_msgs::Odometry& odometry);
+	void publishmsg();
+private:
+	ros::NodeHandle n_;
+	nav_msgs::Odometry odometry_data_;
+	ros::Subscriber odometry_sub_;
+	ros::Publisher odometry_transform_pub_;
+};
+#endif
+*/
