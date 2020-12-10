8df7022f2b11016374f091f624687c9d470b95d9
Christian Mortensen <cnmrtnsn@gmail.com>
2020-12-10 12:16:44 +0100

    Added missing cancel of docking action when forced error

--- unicorn_state_machine/src/unicorn_state_machine/error_state.cpp
+++ unicorn_state_machine/src/unicorn_state_machine/error_state.cpp
@@ -16,6 +16,7 @@ ErrorState::ErrorState(const std::string& error_msg, ros::NodeHandle node) :
 State* ErrorState::run() {
 
     ros::Publisher movebase_cancel_pub = nh_.advertise<actionlib_msgs::GoalID>("/move_base/cancel", 1);
+    ros::Publisher dock_cancel_pub = nh_.advertise<actionlib_msgs::GoalID>("/dock/cancel", 1);
     ros::Publisher lift_cancel_pub = nh_.advertise<actionlib_msgs::GoalID>("/lift/lift_action/cancel", 1);
 
     actionlib_msgs::GoalID cancel_msg;
@@ -25,6 +26,7 @@ State* ErrorState::run() {
 
         movebase_cancel_pub.publish(cancel_msg);
         lift_cancel_pub.publish(cancel_msg);
+        dock_cancel_pub.publish(cancel_msg);
 
         ros::Duration(0.5).sleep();
     } while(ros::ok() && error_);
